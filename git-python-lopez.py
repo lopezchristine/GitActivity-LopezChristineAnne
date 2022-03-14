9daff32bc791fd689ed4e54ce6232ce05d3bd0a4
LopezChristineAnne <lopezchristine019@gmail.com>
2022-03-14 19:22:42 +0800

    My Educational Background added

---
+++ git-python-lopez.py
@@ -0,0 +1,6 @@
+print("Christine Anne S. Lopez")
+print("Pinggan, Gasan, Marinduque")
+print("April 19, 2002")
+print("Marinduque State College")
+print("BS Information Technology")
+print("2nd year, Section: D")
